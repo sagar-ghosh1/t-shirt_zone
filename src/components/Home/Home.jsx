e9cea9866297793e779c29299a00c34cfb6c3838
sagar-ghosh1 <sagorghosh520@gmail.com>
2023-05-02 16:43:48 +0600

    images tshirt

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import TShirt from '../TShirt/TShirt';
+import Cart from '../Cart/Cart';
+import './Home.css';
 
 const Home = () => {
     const tshirts = useLoaderData();
+    const [cart, setCart] = useState([])
+
+    const handleAddToCart = tshirt => {
+        console.log(tshirt)
+    }
+
     return (
-        <div>
-            <h2>This is Home: {tshirts.length}</h2>
+        <div className='home-container'>
+            <div className='t-shirts-container'>
+                {
+                    tshirts.map(tshirt => <TShirt
+                        key={tshirt._id}
+                        tshirt={tshirt}
+                        handleAddToCart={handleAddToCart}
+                    ></TShirt>)
+                }
+            </div>
+            <div className="cart-container">
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
