1a458af55df026e5d8099156eae01d67e2e8a0b7
sagar-ghosh1 <sagorghosh520@gmail.com>
2023-05-04 18:53:25 +0600

    operator

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({ cart, handleRemoveFromCart }) => {
+    let message;
+    if (cart.length === 0) {
+        message = <p>Please add some product</p>
+    }
+    else {
+        message = <div>
+            <h3>Blogeded</h3>
+            <p><small>Thanks for money</small></p>
+        </div>
+    }
     return (
         <div>
-            <h2>Order summary {cart.length}</h2>
+            <h2 className={cart.length === 1 ? 'blue' : 'red'}>Order summary {cart.length}</h2>
+            <p className={`bold ${cart.length === 3 ? 'yellow' : 'purple'}`}>somthing</p>
+            {cart.length > 2 ? <span className='purple'>Aro kino</span> : <span>fokire</span>}
+            {message}
             {
                 cart.map(tshirt => <p
                     key={tshirt._id}
@@ -13,8 +27,28 @@ const Cart = ({ cart, handleRemoveFromCart }) => {
 
                 >x</button> </p>)
             }
+            {
+                cart.length === 2 && <p>Double bonanza</p>
+            }
+            {
+                cart.length === 3 || <h3>Tinta to holo na</h3>
+            }
         </div>
     );
 };
 
 export default Cart;
+
+
+/**
+ * CONDITIONAL RENDERING
+ * 1. use if else to set a variable that will contain an element, components
+ * 2. ternary operator: condition ? 'for true' : 'false'
+ * 3. Logical &&: (if the condition is true then the next thing will be display)
+ * 4. Logical || (if the condition is false then the next thing will be displayed)
+ * **/
+
+/**
+ * CONDITIONAL CSS Class
+ *
+ * **/
